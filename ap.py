bbbd1367322b950e002f35e80c6f925e611c4a2d
misalushin <168902902+misalushin@users.noreply.github.com>
2024-05-04 15:42:53 +0500

    gggg

---
+++ ap.py
@@ -0,0 +1,2 @@
+a = float(input('Qalegen sanin kvadratin tawip beremen san jaz  :'))
+print(f'{a**2} boladi ')
---
+++ app.py
@@ -0,0 +1,20 @@
+# # list-dizim
+# bazarliq['cola', 'may', 'konfet']
+# bahasi[12000, 18000, 15000]
+
+# print(f' men bazardan {bazarliq[0]aldim})
+#       print(bahasi[0] + bahas
+# klass = ['Farxad' , 'Aytmurat' , 'Elektrik' ]
+# print(klass)
+# del klass[0]
+# print(klass)
+# malbazar=['ikram','farxad']
+# print(malbazar)
+# malbazardanaldim=malbazar.pop(1)
+# print(malbazardanaldim)
+# klass = [f'Nelufar,\n Ayjamal , \n Jamila  ,\n Rasul , \n Farxad ,\n Guzal ]
+# print(klass.sorted())
+# SANLAR = list(range(1, 1000000))
+# print(SANLAR)
+a = input(klastagi , narkomanlardin ,ati?  :)
+print(a)
